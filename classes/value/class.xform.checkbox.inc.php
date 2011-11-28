516eb6aa98547b702d62eed28236f3285b042bf4
Jan Kristinus <jan.kristinus@yakamara.de>
2011-11-28 20:44:16 +0100

    checkbox - default status fixed

--- classes/value/class.xform.checkbox.inc.php
+++ classes/value/class.xform.checkbox.inc.php
@@ -9,14 +9,19 @@ class rex_xform_checkbox extends rex_xform_abstract
 
     $checked = "";
 
-    if($this->getValue() == $this->getElement(3)) {
+    if($this->params["send"] != 1 && $this->getValue() === "" && $this->getElement(4) == $this->getElement(3))
+    {
+      $checked = ' checked="checked"';
+      $v = $this->getElement(3);
+
+    }elseif($this->getValue() == $this->getElement(3)) {
       $checked = ' checked="checked"';
       $v = $this->getElement(3);
 
     }elseif($this->getValue() == 1) {
       $checked = ' checked="checked"';
       $v = 1;
-      	
+
     }else {
       $this->setValue("0");
       $v = 1;
@@ -28,10 +33,10 @@ class rex_xform_checkbox extends rex_xform_abstract
     }
 
     $this->params["form_output"][$this->getId()] = '
-			<p class="formcheckbox formlabel-'.$this->getName().'" id="'.$this->getHTMLId().'">
-				<input type="checkbox" class="checkbox '.$wc.'" name="'.$this->getFieldName().'" id="'.$this->getFieldId().'" value="'.$v.'" '.$checked.' />
-				<label class="checkbox '.$wc.'" for="'.$this->getFieldId().'" >'.rex_translate($this->getElement(2)).'</label>
-			</p>';
+      <p class="formcheckbox formlabel-'.$this->getName().'" id="'.$this->getHTMLId().'">
+        <input type="checkbox" class="checkbox '.$wc.'" name="'.$this->getFieldName().'" id="'.$this->getFieldId().'" value="'.$v.'" '.$checked.' />
+        <label class="checkbox '.$wc.'" for="'.$this->getFieldId().'" >'.rex_translate($this->getElement(2)).'</label>
+      </p>';
 
     $this->params["value_pool"]["email"][$this->getElement(1)] = stripslashes($this->getValue());
     if ($this->getElement(5) != "no_db") { $this->params["value_pool"]["sql"][$this->getElement(1)] = $this->getValue(); }
@@ -50,12 +55,12 @@ class rex_xform_checkbox extends rex_xform_abstract
             'type' => 'value',
             'name' => 'checkbox',
             'values' => array(
-                array( 'type' => 'name',   'label' => 'Name' ),
-                array( 'type' => 'text',    'label' => 'Bezeichnung'),
-                array( 'type' => 'text',    'label' => 'Wert wenn angeklickt', 'default' => 1),
-                array( 'type' => 'boolean', 'label' => 'Defaultstatus',         'default' => 1),
-                array( 'type' => 'no_db',   'label' => 'Datenbank',  'default' => 1),
-              ),
+    array( 'type' => 'name',   'label' => 'Name' ),
+    array( 'type' => 'text',    'label' => 'Bezeichnung'),
+    array( 'type' => 'text',    'label' => 'Wert wenn angeklickt', 'default' => 1),
+    array( 'type' => 'boolean', 'label' => 'Defaultstatus',         'default' => 1),
+    array( 'type' => 'no_db',   'label' => 'Datenbank',  'default' => 1),
+    ),
             'description' => 'Ein Selectfeld mit festen Definitionen.',
             'dbtype' => 'varchar(255)',
             'famous' => TRUE
