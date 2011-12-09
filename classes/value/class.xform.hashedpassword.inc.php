ac5260a9da69cad25642371ba5059a06a975a089
Markus Lorch <m.lorch@it-kult.de>
2011-12-09 15:01:17 +0100

    New Class for passwords -> better version

--- classes/value/class.xform.hashedpassword.inc.php
+++ classes/value/class.xform.hashedpassword.inc.php
@@ -11,43 +11,41 @@ class rex_xform_hashedpassword extends rex_xform_abstract
 		$value = $this->getValue();
 		
 		if(!is_array($value))
-			$value = array("new" => "", "hash" => $this->getValue());
+			$value = array('new' => '', 'current' => $this->getValue());
 		
 		$wc = "";
-		if (isset($this->params["warning"][$this->getId()])) {
+		if (isset($this->params["warning"][$this->getId()]))
 			$wc = " ".$this->params["warning"][$this->getId()];
-		}
 
 		$this->params["form_output"][$this->getId()] = '
 			<p class="formtext formlabel-'.$this->getName().'" id="'.$this->getHTMLId().'-0">
 				<label class="text' . $wc . '" for="' . $this->getFieldId() . '-0" >Neues Passwort:</label>
-				<input type="text" class="text'.$classes.$wc.'" name="'.$this->getFieldName().'[new]" id="'.$this->getFieldId().'-0" value="" />
-				<input type="hidden" name="'.$this->getFieldName().'[hash]" id="'.$this->getFieldId().'-1" value="'.htmlspecialchars(stripslashes($value['hash'])).'" />	
+				<input type="text" class="text'.$classes.$wc.'" name="'.$this->getFieldName().'[new]" id="'.$this->getFieldId().'-0" value="" />	
 			</p>';
 		
 		if($this->getElement(4))
 		{
 			$this->params["form_output"][$this->getId()] .= '
-			<p class="formtext formlabel-'.$this->getName().'" id="'.$this->getHTMLId().'-2">
-				<label class="text" for="' . $this->getFieldId() . '-2" >'.$this->getElement(2).' ['.$this->getElement(3).']</label>
-				<input type="text" class="text'.$classes.'" id="'.$this->getFieldId().'-2" value="'.htmlspecialchars(stripslashes($value['hash'])).'" disabled="disabled" />
+			<p class="formtext formlabel-'.$this->getName().'" id="'.$this->getHTMLId().'-1">
+				<label class="text" for="' . $this->getFieldId() . '-1" >'.$this->getElement(2).' ['.$this->getElement(3).']</label>
+				<input type="text" class="text'.$classes.'" id="'.$this->getFieldId().'-1" name="'.$this->getFieldName().'[current]" value="'.htmlspecialchars(stripslashes($value['current'])).'" readonly="readonly" />
 			</p>';
 		}
 
+		## Unverschlüsselt in eMail schreiben
 		$this->params["value_pool"]["email"][$this->getElement(1)] = stripslashes($value['new']);
 		
+		## Verschlüsselt in Datenbank schreiben
 		if ($this->getElement(5) != "no_db")
 		{
 			if($value['new'] != '')
 				$this->params["value_pool"]["sql"][$this->getElement(1)] = hash($this->getElement(3),$value['new']);
-			else
-				$this->params["value_pool"]["sql"][$this->getElement(1)] = $value['hash'];
 		}
 	}
 
 	function getDescription()
 	{
-		return "hashpassword -> Beispiel: hashedpassword|label|Bezeichnung|(md5/sha1/sha512/...)|hashwert anzeigen (1/0)|[no_db]";
+		return "hashedpassword -> Beispiel: hashedpassword|label|Bezeichnung|(md5/sha1/sha512/...)|hashwert anzeigen (1/0)|[no_db]";
 	}
 
 	function getDefinitions()
@@ -67,7 +65,6 @@ class rex_xform_hashedpassword extends rex_xform_abstract
 						'dbtype' => 'text',
 						'famous' => FALSE
 						);
-
 	}
 }
 
