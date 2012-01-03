67e5983f6cde669475170ff0562a2da8b15a9aa1
Markus Lorch <m.lorch@it-kult.de>
2012-01-03 11:23:30 +0100

    Remove warning in generate_password class

--- classes/value/class.xform.generate_password.inc.php
+++ classes/value/class.xform.generate_password.inc.php
@@ -5,7 +5,7 @@ class rex_xform_generate_password extends rex_xform_abstract
 
 	function enterObject()
 	{
-		$this->setValue(substr(md5(microtime().rand(1000)), 0, 6));
+		$this->setValue(substr(md5(microtime().rand(1000,getrandmax())), 0, 6));
 		$this->params["value_pool"]["email"][$this->getElement(1)] = stripslashes($this->getValue());
 		if ($this->getElement(2) != "no_db") $this->params["value_pool"]["sql"][$this->getElement(1)] = $this->getValue();
 	}
