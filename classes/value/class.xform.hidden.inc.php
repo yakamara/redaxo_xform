eb6ba02f516ba416783f38dba4c0a9c19c952c0a
Jan Kristinus <jan.kristinus@yakamara.de>
2012-06-11 02:52:31 +0200

    hidden fields können nun validiert werden

--- classes/value/class.xform.hidden.inc.php
+++ classes/value/class.xform.hidden.inc.php
@@ -3,33 +3,38 @@
 class rex_xform_hidden extends rex_xform_abstract
 {
 
-  function enterObject()
+  public function setValue($value)
   {
-
     if ($this->getElement(3)=="REQUEST" && isset($_REQUEST[$this->getName()]))
     {
-      $this->setValue(stripslashes(rex_request($this->getName())));
-      $this->params["form_output"][$this->getId()] = "\n".'<input type="hidden" name="'.$this->getName().'" id="'.$this->getHTMLId().'" value="'.htmlspecialchars($this->getValue()).'" />';
+      $this->value = stripslashes(rex_request($this->getName()));
 
     }else
     {
-      $this->setValue($this->getElement(2));
-      $this->params["value_pool"]["email"][$this->getName()] = $this->getValue();
-    }
+      $this->value = $this->getElement(2);
 
-    $this->params["value_pool"]["email"][$this->getName()] = stripslashes($this->getValue());
+    }
+    
+  }
+
+  public function enterObject()
+  {
+
+    $this->params["form_output"][$this->getId()] = "\n".'<input type="hidden" name="'.$this->getName().'" id="'.$this->getHTMLId().'" value="'.htmlspecialchars($this->getValue()).'" />';
+
+    $this->params["value_pool"]["email"][$this->getName()] = $this->getValue();
     if ($this->getElement(4) != "no_db")
       $this->params["value_pool"]["sql"][$this->getName()] = $this->getValue();
   }
 
-  function getDescription()
+  public function getDescription()
   {
     return "
-        hidden -> Beispiel: hidden|status|default_value||[no_db]<br />	hidden -> Beispiel: hidden|job_id|default_value|REQUEST|[no_db]
+        hidden -> Beispiel: hidden|status|(default)value||[no_db]<br />	hidden -> Beispiel: hidden|job_id|my_id|REQUEST|[no_db]
     ";
   }
 
-  function getLongDescription()
+  public function getLongDescription()
   {
     return '
     Hiermit können Werte fest als Wert zum Formular eingetragen werden z.B.
