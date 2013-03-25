e739ea00c587848a6f0a881e0019e538f5eff309
Jan Kristinus <jan.kristinus@yakamara.de>
2013-03-25 16:53:01 +0100

    validate uniqueform funktionierte nicht

--- classes/value/class.xform.uniqueform.inc.php
+++ classes/value/class.xform.uniqueform.inc.php
@@ -8,29 +8,26 @@ class rex_xform_uniqueform extends rex_xform_abstract
 
     $table = $this->getElement(2);
 
-    if (!$this->params["send"])
-    {
+    if (!$this->params["send"]) {
       $this->setValue(md5($_SERVER["REMOTE_ADDR"].time()));
 
-    }else
-    {
+    }else {
       $sql = 'select '.$this->getName().' from '.$table.' WHERE '.$this->getName().'="'.$this->getValue().'" LIMIT 1';
       $cd = rex_sql::factory();
       if ($this->params["debug"]) $cd->debugsql = true;
+      
       $cd->setQuery($sql);
       if ($cd->getRows()==1)
       {
-        $this->params["warning"][] = $this->getElement(3);
-        $this->params["warning_messages"][] = $this->getElement(3);
+        $this->params["warning"][$this->getId()] = $this->getElement(3);
+        $this->params["warning_messages"][$this->getId()] = $this->getElement(3);
       }
 
     }
 
-    $this->params["form_output"][$this->getId()] = '<input type="hidden" name="FORM['.$this->params["form_name"].'][el_'.$this->getId().']" value="'.htmlspecialchars(stripslashes($this->getValue())).'" />';
+    $this->params["form_output"][$this->getId()] = '<input type="hidden" name="'.$this->getFieldName().'" value="'.htmlspecialchars(stripslashes($this->getValue())).'" />';
     $this->params["value_pool"]["email"][$this->getName()] = stripslashes($this->getValue());
     $this->params["value_pool"]["sql"][$this->getName()] = stripslashes($this->getValue());
-
-    return;
 
   }
 
