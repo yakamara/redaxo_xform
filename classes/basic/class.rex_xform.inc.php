85b4d3a8d8805d5934c63d5e2c22672c5fcfe602
Gregor Harlan <gregor.harlan@yakamara.de>
2014-05-07 15:21:48 +0200

    Fehlermeldung vermeiden

--- classes/basic/class.rex_xform.inc.php
+++ classes/basic/class.rex_xform.inc.php
@@ -562,19 +562,7 @@ class rex_xform
 
     static function unhtmlentities($text)
     {
-        if (!function_exists('unhtmlentities')) {
-            function unhtmlentities($string)
-            {
-                // Ersetzen numerischer Darstellungen
-                $string = preg_replace('~&#x([0-9a-f]+);~ei', 'chr(hexdec("\\1"))', $string);
-                $string = preg_replace('~&#([0-9]+);~e', 'chr("\\1")', $string);
-                // Ersetzen benannter Zeichen
-                $trans_tbl = get_html_translation_table(HTML_ENTITIES);
-                $trans_tbl = array_flip($trans_tbl);
-                return strtr($string, $trans_tbl);
-            }
-        }
-        return unhtmlentities($text);
+        return html_entity_decode($text);
     }
 
 
