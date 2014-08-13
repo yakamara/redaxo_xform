59aa1d8716b41133948fcfa17c04cf50ba0bd97b
Gregor Harlan <gregor.harlan@yakamara.de>
2014-08-13 18:00:13 +0200

    Prio-Klasse: Verbesserungen bei Verwendung von Beschränkungsfeldern

--- plugins/manager/classes/value/class.xform.prio.inc.php
+++ plugins/manager/classes/value/class.xform.prio.inc.php
@@ -8,6 +8,7 @@
 
 class rex_xform_prio extends rex_xform_abstract
 {
+    private $preEditScopeWhere;
 
     function enterObject()
     {
@@ -15,41 +16,47 @@ class rex_xform_prio extends rex_xform_abstract
 
         $options[1] = $I18N->msg('xform_prio_top');
 
-        $sql = rex_sql::factory();
-        $fields = $this->getElement('fields');
-        if (!is_array($fields)) {
-            $fields = array_filter(explode(',', $fields));
-        }
-        if (empty($fields)) {
-            $fields = array('id');
-        }
-        $selectFields = array();
-        foreach ($fields as $field) {
-            $selectFields[] = '`' . $sql->escape($field) . '`';
-        }
-        $sql->setQuery(sprintf(
-            'SELECT id, %s, `%s` as prio FROM `%s`%s ORDER BY `%2$s`',
-            implode(', ', $selectFields),
-            $sql->escape($this->getElement('name')),
-            $sql->escape($this->params['main_table']),
-            $this->getScopeWhere()
-        ));
-        $prio = 1;
-        while ($sql->hasNext()) {
-            if ($sql->getValue('id') != $this->params['main_id']) {
-                $prio = $sql->getValue('prio') + 1;
-                $label = array();
-                foreach ($fields as $field) {
-                    $label[] = rex_translate($sql->getValue($field), $I18N, false);
-                }
-                $options[$prio] = $I18N->msg('xform_prio_after', implode(' | ', $label));
+        $scopeWhere = $this->getScopeWhere();
+        if (false === $scopeWhere) {
+            $options[''] = $I18N->msg('xform_prio_bottom');
+        } else {
+            $this->preEditScopeWhere = $scopeWhere;
+            $sql = rex_sql::factory();
+            $fields = $this->getElement('fields');
+            if (!is_array($fields)) {
+                $fields = array_filter(explode(',', $fields));
             }
-            $sql->next();
+            if (empty($fields)) {
+                $fields = array('id');
+            }
+            $selectFields = array();
+            foreach ($fields as $field) {
+                $selectFields[] = '`' . $sql->escape($field) . '`';
+            }
+            $sql->setQuery(sprintf(
+                'SELECT id, %s, `%s` as prio FROM `%s`%s ORDER BY `%2$s`',
+                implode(', ', $selectFields),
+                $sql->escape($this->getElement('name')),
+                $sql->escape($this->params['main_table']),
+                $scopeWhere
+            ));
+            $prio = 1;
+            while ($sql->hasNext()) {
+                if ($sql->getValue('id') != $this->params['main_id']) {
+                    $prio = $sql->getValue('prio') + 1;
+                    $label = array();
+                    foreach ($fields as $field) {
+                        $label[] = rex_translate($sql->getValue($field), $I18N, false);
+                    }
+                    $options[$prio] = $I18N->msg('xform_prio_after', implode(' | ', $label));
+                }
+                $sql->next();
+            }
         }
 
         if (!$this->params['send'] && $this->getValue() == '') {
             if ($this->getElement('default') == '') {
-                $this->setValue($prio);
+                $this->setValue(isset($prio) ? $prio : '');
             } else {
                 $this->setValue($this->getElement('default'));
             }
@@ -93,13 +100,23 @@ class rex_xform_prio extends rex_xform_abstract
     function postAction()
     {
         $sql = rex_sql::factory();
-        $sql->setQuery('SET @count = 0');
 
+        $scopeWhere = $this->getScopeWhere();
+        if (!is_null($this->preEditScopeWhere) && $scopeWhere !== $this->preEditScopeWhere) {
+            $this->setValue($this->getElement('default'));
+        }
+
+        if ('' === $this->getValue()) {
+            $order = 'IF(`id` = %d, 1, 0), `%2$s`';
+        } else {
+            $order = '`%2$s`, IF(`id` = %d, 0, 1)';
+        }
+        $sql->setQuery('SET @count = 0');
         $sql->setQuery(sprintf(
-            'UPDATE `%s` SET `%s` = (SELECT @count := @count + 1)%s ORDER BY `%2$s`, IF(`id` = %d, 0, 1)',
+            'UPDATE `%s` SET `%s` = (SELECT @count := @count + 1)%s ORDER BY ' . $order,
             $sql->escape($this->params['main_table']),
             $sql->escape($this->getElement('name')),
-            $this->getScopeWhere(),
+            $scopeWhere,
             $this->params['main_id']
         ));
     }
@@ -119,7 +136,7 @@ class rex_xform_prio extends rex_xform_abstract
                 $value = $this->params['value_pool']['sql'][$column];
             } elseif (isset($this->params['sql_object']) && $this->params['sql_object']->hasValue($column)) {
                 $value = $this->params['sql_object']->getValue($column);
-            } else {
+            } elseif ($this->params['main_id'] > 0) {
                 $sql = rex_sql::factory();
                 $sql->setQuery(sprintf(
                     'SELECT `%s` FROM `%s` WHERE id = %d',
@@ -128,6 +145,9 @@ class rex_xform_prio extends rex_xform_abstract
                     $this->params['main_id']
                 ));
                 $value = $sql->getValue($column);
+            }
+            if (!isset($value)) {
+                return false;
             }
             $where[] = sprintf('`%s` = "%s"', mysql_real_escape_string($column), mysql_real_escape_string($value));
         }
