d56adb63ee4ed4656e77e65f5156a4ec7ad581a2
Gregor Harlan <gregor.harlan@yakamara.de>
2014-08-30 12:57:08 +0200

    Manager: Navigation erst in ADDONS_INCLUDED bauen
    So haben Addons, die erst nach XForm geladen werden, noch die Möglichkeit Sprachdateien zu laden.

--- plugins/manager/config.inc.php
+++ plugins/manager/config.inc.php
@@ -39,29 +39,34 @@ if ($REX['REDAXO'] && !$REX['SETUP']) {
 
     rex_register_extension('OOMEDIA_IS_IN_USE', 'rex_xform_manager::checkMediaInUse');
 
-    $tables = rex_xform_manager_table_api::getTables();
+    rex_register_extension('ADDONS_INCLUDED', function () {
+        global $REX, $I18N;
 
-    $subpages = array();
+        $tables = rex_xform_manager_table_api::getTables();
 
-    foreach ($tables as $table) {
+        $subpages = array();
 
-        $table_perm = 'xform[table:' . $table['table_name'] . ']';
-        $REX['EXTPERM'][] = $table_perm;
+        foreach ($tables as $table) {
 
-        if ($table['status'] == 1 && $table['hidden'] != 1 && $REX['USER'] && ($REX['USER']->isAdmin() || $REX['USER']->hasPerm($table_perm))) {
-            $table_name = rex_translate($table['name']);
+            $table_perm = 'xform[table:' . $table['table_name'] . ']';
+            $REX['EXTPERM'][] = $table_perm;
 
-            if ($I18N) {
-                $I18N->addMsg($table['table_name'], $table_name);
+            if ($table['status'] == 1 && $table['hidden'] != 1 && $REX['USER'] && ($REX['USER']->isAdmin() || $REX['USER']->hasPerm($table_perm))) {
+                $table_name = rex_translate($table['name']);
+
+                if ($I18N) {
+                    $I18N->addMsg($table['table_name'], $table_name);
+                }
+
+                $be_page = new rex_be_page($table_name, array('page' => 'xform', 'subpage' => 'manager', 'tripage' => 'data_edit', 'table_name' => $table['table_name']));
+                $be_page->setHref('index.php?page=xform&subpage=manager&tripage=data_edit&table_name=' . $table['table_name']);
+                $subpages[] = new rex_be_main_page('manager', $be_page);
             }
-
-            $be_page = new rex_be_page($table_name, array('page' => 'xform', 'subpage' => 'manager', 'tripage' => 'data_edit', 'table_name' => $table['table_name']));
-            $be_page->setHref('index.php?page=xform&subpage=manager&tripage=data_edit&table_name=' . $table['table_name']);
-            $subpages[] = new rex_be_main_page($mypage, $be_page);
         }
-    }
 
-    $REX['ADDON']['pages'][$mypage] = $subpages;
+        OOPlugin::setProperty('xform', 'manager', 'pages', $subpages);
+
+    });
 
     // hack - if data edit, then deactivate xform navigation
     if (rex_request('tripage', 'string') == 'data_edit') {
