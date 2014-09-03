b7e7810245d5612412a3145b598d69976d6d7702
Jan Kristinus <jan.kristinus@yakamara.de>
2014-09-03 21:38:46 +0200

    PlugIn wird beim Update nicht richtig erkannt. Deswegen ein direkter Include vom Manager install

--- update.inc.php
+++ update.inc.php
@@ -2,8 +2,6 @@
 
 global $REX;
 
-if (OOPlugin::isInstalled('xform', 'manager')) {
-    include rex_path::plugin('xform', 'manager', 'install.inc.php');
-}
+include rex_path::plugin('xform', 'manager', 'install.inc.php');
 
 $REX['ADDON']['update']['xform'] = true;
