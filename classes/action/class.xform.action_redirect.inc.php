bf46d9e3330444ec94d76fb9be7a06d4c568352a
IngoWinter <post@ingo-winter.de>
2016-03-04 13:22:44 +0100

    exit ergänzt

--- classes/action/class.xform.action_redirect.inc.php
+++ classes/action/class.xform.action_redirect.inc.php
@@ -45,6 +45,7 @@ class rex_xform_action_redirect extends rex_xform_action_abstract
         if ($url != '') {
             ob_end_clean();
             header('Location: ' . $url);
+	        exit;
         }
 
     }
